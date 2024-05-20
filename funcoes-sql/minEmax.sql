00977193cddb3f351221a695652ad5d72fdc1908
Thalita coelho <thalita.bastos94@gmail.com>
2024-05-20 20:56:25 -0300

    max

--- funcoes-sql/minEmax.sql
+++ funcoes-sql/minEmax.sql
@@ -9,4 +9,11 @@ select min(name) from usuarios; -- Aaron Bates
 
 select min(idade) from usuarios; -- 13
 
+--max tras o maior valor que tem no campo que informamos como argumento 
 
+select max(cadastro) from usuarios; -- 2025-05-18 07:57:27
+
+select max(name) from usuarios; -- Zia Carlson
+
+
+select max(idade) from usuarios; -- 80
