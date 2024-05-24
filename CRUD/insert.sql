2b856939bc6aa975e60a8e0fe120a391be871b47
Thalita coelho <thalita.bastos94@gmail.com>
2024-05-24 02:40:18 -0300

    .

--- CRUD/insert.sql
+++ CRUD/insert.sql
@@ -3,15 +3,15 @@
 insert into produtos
 (nome, descricao, preco, categoria)
 values 
-('Camisa', null, 4990, 'Roupas')
+('Camisa', null, 4990, 'Roupas');
 
 -- Inserir vários registros de uma só vez
 
 insert into produtos
 (nome, descricao, preco, categoria)
 values 
-('Camisa', null, 4990, 'Roupas');
-('Calça', null, 11990, 'Roupas');
+('Camisa', null, 4990, 'Roupas'),
+('Calça', null, 11990, 'Roupas'),
 ('Bermuda', null, 8998, 'Roupas');
 
 -- Inserção de registros
