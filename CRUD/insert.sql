66fb8d026afbcc2d945e55583b2ced356e7d1ac8
Thalita coelho <thalita.bastos94@gmail.com>
2024-05-24 02:38:05 -0300

    insert

---
+++ CRUD/insert.sql
@@ -0,0 +1,17 @@
+-- inserir registros na tabela produtos
+
+insert into produtos
+(nome, descricao, preco, categoria)
+values 
+('Camisa', null, 4990, 'Roupas')
+
+-- Inserir vários registros de uma só vez
+
+insert into produtos
+(nome, descricao, preco, categoria)
+values 
+('Camisa', null, 4990, 'Roupas');
+('Calça', null, 11990, 'Roupas');
+('Bermuda', null, 8998, 'Roupas');
+
+-- Inserção de registros
