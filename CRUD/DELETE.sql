d18cb33673f8a7e346f5c2c9bea612d4a6ff358c
Thalita coelho <thalita.bastos94@gmail.com>
2024-05-27 23:17:35 -0300

    delete

---
+++ CRUD/DELETE.sql
@@ -0,0 +1,10 @@
+--sintaxe para deletar dentro de um bd 
+
+update 
+from produtos
+where id = 2;
+-- deletar o nome e o valor do produto com id 12 
+
+delete 
+from produtos 
+where descricao is null; -- o where é a condição 
