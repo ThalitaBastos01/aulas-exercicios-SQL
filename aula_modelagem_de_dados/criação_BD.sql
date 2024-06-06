77e9cd798659c585b87a362a52b6456244b30e13
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-06 20:53:06 -0300

    tabela endereços

--- aula_modelagem_de_dados/criação_BD.sql
+++ aula_modelagem_de_dados/criação_BD.sql
@@ -32,3 +32,17 @@ values
 
 -- Tabela enderecos - Relacionamento um para um
 
+create table enderecos (
+  id serial primary key,
+  editora_id integer references editoras (id),
+  cep text not null,
+  rua text,
+  bairro text, 
+  cidade text,
+  estado char(2),
+  pais text
+);
+
+insert into enderecos (editora_id, cep)
+values 
+(1, '41000-000');
