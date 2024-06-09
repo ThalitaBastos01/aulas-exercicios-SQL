24a480f1d89da8009624d6f01e4b0d2953f01cb2
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-09 14:00:43 -0300

    inner join

--- aula_conexao_node_pg/index.js
+++ aula_conexao_node_pg/index.js
@@ -7,13 +7,18 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/:id', async (req, res) => {
+app.get('/', async (req, res) => {
     const { id } = req.params
-
+   
     try {
-        const resultado = await pool.query(`select * from empresas where id = ${id}`)
+        //const query = 'select * from empresas where nome = $1 or nome = $2'
+        //const params = ['Google', 'Facebook']
 
-        return res.json(resultado.rows)
+        const query = 'select e.id as empresaId, f.id as filialId, e.nome, f.pais, p.nome as funcionario from empresas e inner join filiais f on e.id = f.empresa_id join pessoas p on e.id = p.empresa_id;'
+       
+
+      const resultado = await pool.query(query)
+       return res.json(resultado.rows)
 
     } catch (error) {
         console.log(error.massage);
@@ -21,3 +26,7 @@ app.get('/:id', async (req, res) => {
 })
 
 app.listen(3000)
+
+//const query = 'select * from empresas where nome = $1 or nome = $2' --- passando mais de 1 parametro.
+
+// clausula JOIN - É uma clausula sql que permite agrupar registros de duas ou mais tabelas em uma unica consulta. : inner join , full join, left join e right join
---
+++ aula_conexao_node_pg/join/InnerJoin.sql
@@ -0,0 +1,5 @@
+
+select * from empresas inner join filiais on empresas.id = filiais.empresa_id; 
+
+
+-- o Inner Join, uma ferramenta útil para unir tabelas com base em um campo de relacionamento.
