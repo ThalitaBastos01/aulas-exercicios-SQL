c255684b92f33f270e0dfd3598af91fa1aea4e0a
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-26 17:45:51 -0300

    .

--- aula-query-builder/index.js
+++ aula-query-builder/index.js
@@ -4,8 +4,10 @@ const knex = require('./conexao')
 const app = express();
 
 app.use(express.json());
+
 app.get('/', async (req, res) => {
-    return res.json('ok')
+    const agenda = await knex('agenda').debug();
+    return res.json(agenda)
 });
 
 app.listen(3000);
