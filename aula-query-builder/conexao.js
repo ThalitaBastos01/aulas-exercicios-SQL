be10d8a3c7bb8bba07621d12700b28881239995f
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-27 17:11:05 -0300

    first e select com knex

--- aula-query-builder/conexao.js
+++ aula-query-builder/conexao.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
       host: 'localhost',
-      port: 3000,
+      port: '5432',
       user: 'postgres',
       password: '174029',
       database: 'knexjs',
--- aula-query-builder/index.js
+++ aula-query-builder/index.js
@@ -6,7 +6,27 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const agenda = await knex('agenda').debug();
+    //conexao.query('select * from "agenda"')
+    // const agenda = await knex('agenda').debug();
+    // const agenda = await knex.raw('select * from "agenda"') // query bruta
+
+    //select com where
+    // const agenda = await knex('agenda').where('id', 5).debug();
+
+    //agora quero que ele busque todos os id seja diferente de 5 
+    // const agenda = await knex('agenda').where('id','!=', 5).debug();
+
+    // buscando como objeto
+    // const agenda = await knex('agenda').where({ id: 5 }).debug();
+
+    //buscando o primeiro registro dentro da consulta que estamos fazendo ultilizamos o first
+    // const agenda = await knex('agenda').where({ id: 5 }).first().debug();
+
+    //agora eu quero retornar mas não com todos os campos usando o select antes do first
+    const agenda = await knex('agenda').where({ id: 5 }).select('id', 'nome').first().debug();
+
+
+
     return res.json(agenda)
 });
 
